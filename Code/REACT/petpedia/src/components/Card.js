52bcd8de956ba6ba6f92d75600ec9f7d7d1f9efe
Frederick Cid <frederickcid@gmail.com>
2022-12-15 16:59:32 -0300

    intentando el search

--- Code/REACT/petpedia/src/components/Card.js
+++ Code/REACT/petpedia/src/components/Card.js
@@ -1,33 +1,68 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import data from "./data.json";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./Card.css";
 
-export default function CardPostComponent() {
+const CardPostComponent = (props) => {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
+  const filteredData = data.filter((el) => {
+    if (data.titulo === '') {
+        return el;
+    } else {
+        return el.titulo.toLowerCase().includes(data.titulo)
+    }
+})
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image=""
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div class="container">
+      <div class="row casillas">
+        <div className="search">
+          <TextField
+            id="outlined-basic"
+            onChange={inputHandler}
+            variant="outlined"
+            fullWidth
+            label="Search"
+            value={inputText}
+          />
+        </div>
+        {data.map((post) => (
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={post.img_url}
+              alt="no_image"
+            />
+            <CardContent>
+              <Typography  gutterBottom variant="h5" component="div">
+                {post.titulo}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {post.descripcion}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Compartir</Button>
+              <Button size="small">Leer mas</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </div>
+       
+    </div>
   );
-}
+};
+
+export default CardPostComponent;
--- Code/REACT/petpedia/src/pages/Home.js
+++ Code/REACT/petpedia/src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { CardPostComponent } from "../components";
+import "./Home.css";
 
-
-const Home = () =>{
-    return(<body>
-        <CardPostComponent/>
+const Home = () => {
+  return (
+    <body>
+      <h1>Headder</h1>
+      <div class="grilla">
+        <CardPostComponent />
+      </div>
+      <div>
+        <h2>Footer</h2>
+      </div>
     </body>
-    );
+  );
 };
 
 export default Home;
